ea1f4319d1ec2a7e5c7ad34ccccb15af5458025b
olawalesegun <olawalesegun0@gmail.com>
2024-09-20 21:51:36 +0300

    implemntations complete

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -8,11 +8,12 @@ contract NFTMarketplace is ERC721URIStorage {
   address payable public owner;
   uint256 public allItemsSoldCount;
   uint256 public tokenId;
-  uint256 public costForListing = 10;
+  uint256 public costForListing = 0.01 ether;
 
   struct NFTOrder {
     uint256 id;
     uint256 price;
+    address[] prevOwner;
     address payable seller;
     address payable buyer;
 
@@ -29,22 +30,33 @@ contract NFTMarketplace is ERC721URIStorage {
   error NotAuthorized();
   error PriceMustBeGreaterThanZero();
   error AddressZeroDetected();
+  error NotExactAmount();
+  error InsufficientCostForListing();
+  error NFTNotAvailable();
+
+  event NftPurcaseSuccessful(uint256 _tokenId, address whoPays);
 
   constructor()ERC721("KHALSNFT", "KNFT"){
     owner = payable(msg.sender);
   }
 
-  function listNFT(string memory _tokenURI, uint256 _price) public returns(uint256){
+  // in this function, I have the mint, and sell function
+  function listNFT(string memory _tokenURI, uint256 _price) public payable returns(uint256){
    if(msg.sender == address(0)){
     revert AddressZeroDetected();
    }
    if(_price <= 0){
     revert PriceMustBeGreaterThanZero();
    }
+   if(msg.value != costForListing){
+    revert InsufficientCostForListing();
+   }
    tokenId++;
    uint256 tokId = tokenId;
    _safeMint(msg.sender, tokenId);
    _setTokenURI(tokId, _tokenURI);
+
+   owner.transfer(msg.value);
 
    NFTOrder storage nftList = listedNFTs[tokId];
    nftList.id = tokId;
@@ -52,6 +64,36 @@ contract NFTMarketplace is ERC721URIStorage {
    nftList.price = _price;
 
    return tokId;
+  }
+
+  function buyNFT(uint256 _tokenId) external payable {
+    if(msg.sender == address(0)){
+      revert AddressZeroDetected();
+    }
+
+    NFTOrder storage nftList = listedNFTs[_tokenId];
+
+    if(nftList.price == 0){
+      revert NFTNotAvailable();
+    }
+    uint256 amountToPay = nftList.price + costForListing;
+
+    if(msg.value != amountToPay){
+      revert NotExactAmount();
+    }
+    address oldOwner = nftList.seller;
+    nftList.prevOwner.push(oldOwner);
+    nftList.seller = payable(msg.sender);
+
+    _safeTransfer(oldOwner, msg.sender, _tokenId);
+
+    allItemsSoldCount++;
+    payable(oldOwner).transfer(msg.value);
+
+    delete listedNFTs[_tokenId];
+
+    emit NftPurcaseSuccessful(_tokenId, msg.sender);
+
   }
 
 
